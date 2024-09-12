79ec19900606f901915be5c4a55ec2db212ad1b1
RUBEN M <minaidroborubendarley@gmail.com>
2024-09-12 09:54:53 -0500

    multiplos.1

---
+++ Multiplos.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Multiplos {
+     public static void main(String[] args) {
+     
+        Scanner scanner = new Scanner(System.in);
+
+    
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        String resultado = verificarmultiplos(num1, num2);
+        System.out.println(resultado);
+
+        scanner.close();
+    }
+
+
+    public static String verificarmultiplos(int num1, int num2) {
+        if (num1 == 0 || num2 == 0) {
+            return "Error: no se puede dividir entre cero";
+        }
+        if (num1 % num2 == 0) {
+            return num1 + " es múltiplo de " + num2;
+        } else if (num2 % num1 == 0) {
+            return num2 + " es múltiplo de " + num1;
+        } else {
+            return "Ninguno de los dos es múltiplo del otro";
+        }
+    }
+}
